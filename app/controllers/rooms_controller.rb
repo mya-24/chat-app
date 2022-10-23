e6c26deffe0bc3db4ce9c79e5f87688f5f7723e0
mya-24 <87232931+mya-24@users.noreply.github.com>
2022-10-23 18:59:37 +0900

    チャット画面の編集

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,4 +3,19 @@ class RoomsController < ApplicationController
     @room = Room.new
   end
 
+  def create
+    @room = Room.new(create_rm)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def create_rm
+    params.require(:room).permit(:name, user_ids: [])
+  end
+
 end
